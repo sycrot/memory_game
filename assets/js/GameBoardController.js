5536b4876e28eb295c7f00065022be014874f327
Thiago Farias <thiagofarias99@hotmail.com>
2021-10-26 11:04:48 -0300

    organized imports

--- assets/js/GameBoardController.js
+++ assets/js/GameBoardController.js
@@ -6,38 +6,58 @@ class GameBoard {
     }
 
     init() {
-        const images = [
-            'fas fa-apple-alt',
-            'fas fa-anchor',
-            'fab fa-android',
-            'fab fa-angellist',
-            'fas fa-baby-carriage',
-            'fas fa-basketball-ball',
-            'fas fa-bell',
-            'fas fa-biking',
-            'fas fa-biohazard',
-            'fab fa-bluetooth'
-        ]
+        const imagesRandom = () => {
+            const images = [
+                'fas fa-apple-alt',
+                'fas fa-anchor',
+                'fab fa-android',
+                'fab fa-angellist',
+                'fas fa-baby-carriage',
+                'fas fa-basketball-ball',
+                'fas fa-bell',
+                'fas fa-biking',
+                'fas fa-biohazard',
+                'fab fa-bluetooth',
+                'fas fa-angry',
+                'fas fa-baby',
+                'fas fa-bacteria',
+                'fas fa-bed',
+                'fas fa-beer',
+                'fab fa-bitcoin'
+            ]
+    
+            for (let i = images.length - 1; i > 0; i--) {
+                let j = Math.floor(Math.random() * (i + 1))
+                let temp = images[i]
+    
+                images[i] = images[j]
+                images[j] = temp
+            }
+    
+            return images
+        }
         
-        let cardHTML = ''
-        
-        images.slice(0,gridNumber).forEach(img => {
-            cardHTML += `
-                <div class="memory-card" data-card="${img}">
-                    <i id="front-face" class="${img}"></i>
-                    <i id="back-face" class="fas fa-minus"></i>
-                </div>
-            `
-        })
-
-        cardBoard.innerHTML = cardHTML + cardHTML
+        const getImages = () => {
+            let cardHTML = ''
+            imagesRandom().slice(0,gridNumber).forEach(img => {
+                cardHTML += `
+                    <div class="memory-card" data-card="${img}">
+                        <i id="front-face" class="${img}"></i>
+                        <i id="back-face" class="fas fa-minus"></i>
+                    </div>
+                `
+            })
+    
+            cardBoard.innerHTML = cardHTML + cardHTML
+        }
+        getImages()
         
         const cards = document.querySelectorAll('.memory-card')
         let firstCard, secondCard
         let lockCards = false
         let players = []
 
-        function getPlayers() {
+        const getPlayers = () => {
             let divPlayer = '' 
             
             for (let i=0; i<numberPlayers; i++) {
@@ -66,12 +86,12 @@ class GameBoard {
         
             if (!firstCard) {
                 firstCard = this
-        
+                firstCard.style.pointerEvents = 'none'
                 return false
             }
         
             secondCard = this
-        
+            firstCard.style.pointerEvents = 'visible'
             checkForMatch()
         }
         
@@ -102,19 +122,19 @@ class GameBoard {
             divPlayer[player].classList.add('gc-player-active')
         }
 
-        const memoryCards = cardBoard.querySelectorAll('.memory-card')
         function addPointsPlayer(player) {
             players[player].points += 1
             let pointsPlayer = divPlayer[player].querySelector('.points')
             pointsPlayer.innerHTML = players[player].points
 
-            if (document.querySelectorAll('.memory-card.flip').length >= memoryCards.length) winningPlayer()
+            if (document.querySelectorAll('.memory-card.flip').length >= cards.length) winningPlayer()
         }
 
+        const divWinningPlayer = document.querySelector('.memorygame-winningplayer')
         function winningPlayer() {
-            const divWinningPlayer = document.querySelector('.memorygame-winningplayer')
+            const winningPlayerContent = document.querySelector('.winningplayer-content')
+            const buttonRestart = document.querySelector('.winninplayer-btn')
             let numPoints = []
-            let winningPlayerContent = ''
 
             players.map((e, index) => {
                 numPoints.push(e.points)
@@ -125,30 +145,30 @@ class GameBoard {
             if (winningPlayer.length > 1) {
                 let players = []
                 winningPlayer.forEach(e => players.push(e.id))
-                winningPlayerContent = `
-                    <div class="winnigplayer-content">
-                        <p class="winningplayer-points">com ${winningPlayer[0].points} pontos</p>
-                        <h3 class="winningplayer-title">o Player ${players.join(', e Player ')} <br> empataram!</h3>
-                    </div>
+                winningPlayerContent.innerHTML = `
+                    <p class="winningplayer-points">com ${winningPlayer[0].points} pontos</p>
+                    <h3 class="winningplayer-title">o Player ${players.join(', e Player ')} <br> empataram!</h3>
                 `
-                divWinningPlayer.innerHTML += winningPlayerContent
             } else {
                 winningPlayer.forEach(e => {
-                    winningPlayerContent = `
-                        <div class="winnigplayer-content">
-                            <p class="winningplayer-points">com ${e.points} pontos</p>
-                            <h3 class="winningplayer-title">o Player ${e.id} <br> é o vencedor!</h3>
-                        </div>
+                    winningPlayerContent.innerHTML = `
+                        <p class="winningplayer-points">com ${e.points} pontos</p>
+                        <h3 class="winningplayer-title">o Player ${e.id} <br> é o vencedor!</h3>
                     `
-                    divWinningPlayer.innerHTML += winningPlayerContent
                 })
             }
             
-            setInterval(() => {
+            setTimeout(() => {
                 cardBoard.style.display = 'none'
                 divPlayers.style.display = 'none'
                 divWinningPlayer.style.display = 'flex'
+                buttonRestart.addEventListener('click', restartGame)
             }, 1500)
+        }
+
+        function restartGame() {
+            initContainer.style.display = 'flex'
+            divWinningPlayer.style.display = 'none'
         }
         
         function disableCards() {
